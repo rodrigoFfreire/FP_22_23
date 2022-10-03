53d2a09218e3326ece0fcf4829e199071edc437d
Rodrigo Freire <rodrigofreitasfreire@gmail.com>
2022-10-03 22:25:36 +0100

    Added Part3 Functions defs

---
+++ Projeto 1/Part3 - Sol. Sistema de Eqs Lineares.py
@@ -0,0 +1,19 @@
+def produto_interno(vet1: tuple, vet2: tuple) -> float:
+    pass
+
+
+def verifica_convergencia(matrix: tuple, c: tuple, x: tuple, prec: float) -> bool:
+    pass
+    
+
+def retira_zeros_diagonal(matrix: tuple, c: tuple) -> tuple:
+    pass
+
+
+def eh_diagonal_dominante(matrix: tuple) -> bool:
+    pass
+
+
+def resolve_sistema(matriz: tuple, c: tuple, precision: float) -> tuple:
+    pass
+
