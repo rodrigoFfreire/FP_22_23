5007ce284265095172b5337f36afdc7990200bac
Rodrigo Freire <rodrigofreitasfreire@gmail.com>
2022-10-04 16:24:53 +0100

    Updated Project

--- Projeto1/Part2 - Metodo de hondt.py
+++ Projeto1/Part2 - Metodo de hondt.py
@@ -1,51 +1,65 @@
 def calcula_quocientes(votes: dict, deputies: int) -> dict:
+    '''Calcula os quocientes dos votos dependendo do valor de {deputies}
+    para o método de hondt
+    '''
     vote_copy = votes.copy()
     for i in vote_copy:
         quocientes = []
         for j in range(1, deputies + 1):
             quocientes.append(vote_copy[i] / j)
-        vote_copy.update({i : quocientes})
+        vote_copy.update({i : quocientes})            # Substitui a copia dos votos pelo o valor atualizado
         
     return vote_copy
 
 
 def atribui_mandatos(votes: dict, deputies: int) -> list:
+    '''Retorna a lista dos deputados elegidos identificados pela letra correspondente
+    ao seu partido
+    '''
     votes_copy = votes.copy()
     selected_deps = []
+    quotients = calcula_quocientes(votes, deputies)
     
     for i in range(deputies):
-        new_dep = [p for p, value in votes_copy.items() if value == max(votes_copy.values())]
-        if len(new_dep) > 1:
-            least_voted = min([votes[p] for p in new_dep])
-            new_dep = filter(lambda dep: votes[dep] == least_voted, new_dep)
+        new_letter = [p for p, value in votes_copy.items() if value == max(votes_copy.values())]  # Nota 1 (Consultar no fim do ficheiro)
+        if len(new_letter) > 1:                                                                   # No caso de haver partidos com o mesmo num de votos
+            least_voted = min([votes[p] for p in new_letter])                                     # Nota 2 (Consultar no fim do ficheiro)
+            new_letter = filter(lambda dep: votes[dep] == least_voted, new_letter)                # Nota 3 (Consultar no fim do ficheiro)
            
-        new_dep = ''.join(new_dep) 
-        votes_copy[new_dep] = votes[new_dep] / ((votes[new_dep] / votes_copy[new_dep]) + 1)
-        selected_deps.append(new_dep) 
+        new_letter = ''.join(new_letter) 
+        votes_copy[new_letter] = quotients[new_letter][                                             
+            quotients[new_letter].index(votes_copy[new_letter]) + 1                               # Nota 4 (Consultar no fim do ficheiro)
+            ]   
+        selected_deps.append(new_letter) 
                  
     return selected_deps
 
 
 def obtem_partidos(votes: dict) -> list:
+    '''Retorna a lista de todos os partidos'''
     parties = [j for i in votes.values() for j in i['votos']]
     
-    return list(dict.fromkeys(parties))
+    return list(dict.fromkeys(parties))    # Ao fazer esta conversao eliminamos elementos duplicados 
 
 
 def obtem_resultado_eleicoes(votes: dict) -> list:
+    '''Retorna os resultados das eleições
+    O numero de votos totais de cada partido
+    O numero de deputados de cada partido'''
     if not isinstance(votes, dict) or not votes:
-        raise ValueError('obtem_resultado_eleicoes: argumento invalido')
+        raise ValueError('obtem_resultado_eleicoes: argumento invalido')            # Erro se o argumento nao for dict
     
-    for i in votes.values():
-        if not 'votos' in i.keys() or not 'deputados' in i.keys():
-            raise ValueError('obtem_resultado_eleicoes: argumento invalido')
-        
-        if (not isinstance(list(i.keys())[0], str)
-            or not isinstance(list(i.keys())[1], str)
-            or not isinstance(i['votos'], dict) 
-            or not isinstance(i['deputados'], int)
-            or not any(i['votos'].values())
-            or i['deputados'] < 1
+    for j, i in votes.items():                                
+        if (not isinstance(j, str)
+            or not isinstance(i, dict) 
+            or not 'votos' in i.keys()                                                 # Erro se nao existir a key 'votos'
+            or not 'deputados' in i.keys()                                          # Erro se nao existir a key 'deputados'
+            #or not isinstance(list(i.keys())[0], str)                               # Erro se a key 'deputados' nao for str
+            #or not isinstance(list(i.keys())[1], str)                               # Erro se a key 'votos' nao for str
+            or not isinstance(i['votos'], dict)                                     # Erro se nao existir a key 'votos'
+            or not isinstance(i['deputados'], int)                                  # Erro se nao existir a key 'votos'
+            or not any(i['votos'].values())                                         # Erro se nao existir a key 'votos'
+            or i['deputados'] < 1                                                   # Erro se nao existir a key 'votos'
             ):
             raise ValueError('obtem_resultado_eleicoes: argumento invalido')
         
@@ -71,3 +85,13 @@ def obtem_resultado_eleicoes(votes: dict) -> list:
     results.sort(key=lambda party: party[2], reverse=True)
     
     return results
+
+# info = {
+#             'Endor':   {69: 7, 
+#                         'votos': {'A':12000, 'B':7500, 'C':5250, 'D':3000}},
+#             'Hoth':    {'deputados': 6, 
+#                         'votos': {'A':9000, 'B':11500, 'D':1500, 'E':5000}},
+#             'Tatooine': {'deputados': 3, 
+#                         'votos': {'A':3000, 'B':1900}}}
+
+#print(obtem_resultado_eleicoes(info))
--- Projeto1/projeto1-main.py
+++ Projeto1/projeto1-main.py
@@ -108,53 +108,67 @@ def justifica_texto(text: str, length: int) -> tuple:
 ###################################
 
 def calcula_quocientes(votes: dict, deputies: int) -> dict:
+    '''Calcula os quocientes dos votos dependendo do valor de {deputies}
+    para o método de hondt
+    '''
     vote_copy = votes.copy()
     for i in vote_copy:
         quocientes = []
         for j in range(1, deputies + 1):
             quocientes.append(vote_copy[i] / j)
-        vote_copy.update({i : quocientes})
+        vote_copy.update({i : quocientes})            # Substitui a copia dos votos pelo o valor atualizado
         
     return vote_copy
 
 
 def atribui_mandatos(votes: dict, deputies: int) -> list:
+    '''Retorna a lista dos deputados elegidos identificados pela letra correspondente
+    ao seu partido
+    '''
     votes_copy = votes.copy()
     selected_deps = []
+    quotients = calcula_quocientes(votes, deputies)
     
     for i in range(deputies):
-        new_dep = [p for p, value in votes_copy.items() if value == max(votes_copy.values())]
-        if len(new_dep) > 1:
-            least_voted = min([votes[p] for p in new_dep])
-            new_dep = filter(lambda dep: votes[dep] == least_voted, new_dep)
+        new_letter = [p for p, value in votes_copy.items() if value == max(votes_copy.values())]  # Nota 1 (Consultar no fim do ficheiro)
+        if len(new_letter) > 1:                                                                   # No caso de haver partidos com o mesmo num de votos
+            least_voted = min([votes[p] for p in new_letter])                                     # Nota 2 (Consultar no fim do ficheiro)
+            new_letter = filter(lambda dep: votes[dep] == least_voted, new_letter)                # Nota 3 (Consultar no fim do ficheiro)
            
-        new_dep = ''.join(new_dep) 
-        votes_copy[new_dep] = votes[new_dep] / ((votes[new_dep] / votes_copy[new_dep]) + 1)
-        selected_deps.append(new_dep) 
+        new_letter = ''.join(new_letter) 
+        votes_copy[new_letter] = quotients[new_letter][                                             
+            quotients[new_letter].index(votes_copy[new_letter]) + 1                               # Nota 4 (Consultar no fim do ficheiro)
+            ]   
+        selected_deps.append(new_letter) 
                  
     return selected_deps
 
 
 def obtem_partidos(votes: dict) -> list:
+    '''Retorna a lista de todos os partidos'''
     parties = [j for i in votes.values() for j in i['votos']]
     
-    return list(dict.fromkeys(parties))
+    return list(dict.fromkeys(parties))    # Ao fazer esta conversao eliminamos elementos duplicados 
 
 
 def obtem_resultado_eleicoes(votes: dict) -> list:
+    '''Retorna os resultados das eleições
+    O numero de votos totais de cada partido
+    O numero de deputados de cada partido'''
     if not isinstance(votes, dict) or not votes:
-        raise ValueError('obtem_resultado_eleicoes: argumento invalido')
+        raise ValueError('obtem_resultado_eleicoes: argumento invalido')            # Erro se o argumento nao for dict
     
-    for i in votes.values():
-        if not 'votos' in i.keys() or not 'deputados' in i.keys():
-            raise ValueError('obtem_resultado_eleicoes: argumento invalido')
-        
-        if (not isinstance(list(i.keys())[0], str)
-            or not isinstance(list(i.keys())[1], str)
-            or not isinstance(i['votos'], dict) 
-            or not isinstance(i['deputados'], int)
-            or not any(i['votos'].values())
-            or i['deputados'] < 1
+    for j, i in votes.items():                                
+        if (not isinstance(j, str)
+            or not isinstance(i, dict) 
+            or not 'votos' in i.keys()                                                 # Erro se nao existir a key 'votos'
+            or not 'deputados' in i.keys()                                          # Erro se nao existir a key 'deputados'
+            #or not isinstance(list(i.keys())[0], str)                               # Erro se a key 'deputados' nao for str
+            #or not isinstance(list(i.keys())[1], str)                               # Erro se a key 'votos' nao for str
+            or not isinstance(i['votos'], dict)                                     # Erro se nao existir a key 'votos'
+            or not isinstance(i['deputados'], int)                                  # Erro se nao existir a key 'votos'
+            or not any(i['votos'].values())                                         # Erro se nao existir a key 'votos'
+            or i['deputados'] < 1                                                   # Erro se nao existir a key 'votos'
             ):
             raise ValueError('obtem_resultado_eleicoes: argumento invalido')
         
@@ -180,3 +194,30 @@ def obtem_resultado_eleicoes(votes: dict) -> list:
     results.sort(key=lambda party: party[2], reverse=True)
     
     return results
+
+
+
+
+
+
+
+########
+# NOTAS (Comentarios muitos grandes para caber ao lado de uma linha)
+########
+
+# NOTA 1: É adicionado um novo elemento p à lista quando encontrar o valor maximo de votos atual
+#           correspondente a um dado circulo eleitoral. Ex: {'A': 12000, 'B': 7000, 'C': 3000} ->
+#           -> 'A' é adicionado à lista new_letter pois o seu valor é o máximo
+
+# NOTA 2: Esta linha serve para descobrir dos partidos com votos iguais o que inicialmente foi menos votado
+#           .Ex {'A': 12000, 'B': 6000, 'C': 3000}  (Supondo que A foi escolhido) -> {'A': 6000, 'B': 6000, 'C': 3000} ->
+#           -> least_voted = 'B' porque entre A e B, B foi o partido com menos votos
+
+# NOTA 3: Depois da linha acima ter sido executada, precisamos de filtrar a lista new_letter e retirar todas as
+#           letras exceto a least_voted. Utiliza-se a funcao filter().
+#               Argumento 1: funcao anonima (lambda) que quando retorna falsa retira o elemento da lista
+#                             que fez com que a funcao retornasse FALSE
+#               Argumento 2: A lista em questão
+
+# NOTA 4: O valor apos dividir os votos de um determinado partido é igual ao valor da lista de quocientes
+#           dada por calcula_quocientes() cujo indice é o indice do valor atual + 1 
