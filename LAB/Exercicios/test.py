bdc7dbd2d2545b7fbc62de63cb7ccd3bbde2bd70
Rodrigo Freire <rodrigofreitasfreire@gmail.com>
2022-10-07 20:35:54 +0100

    updated project - new retira_zeros_diagonal func

--- LAB/Exercicios/test.py
+++ LAB/Exercicios/test.py
@@ -1,11 +1,17 @@
-print("Introduza um número")
-num = eval(input("? "))
+matrix = ((0, 1, 1, 1), (1, 0, 1, 1), (1, 1, 0, 1), (1, 1, 1, 0))
 
-counter = 0
-while num != 0:
-    digit = num % 10
-    num //= 10  # num = num // 10
-    if digit == 0 and num % 10 == 0:
-        counter += 1
 
-print("O numero tem", counter, "zeros seguidos")
+matrix_temp = ()
+for l in range(len(matrix)):
+    if matrix[l][l] == 0:
+        for j in range(l + 1, len(matrix)):
+            if matrix[j][l] != 0 and j == len(matrix) - 1:
+                print(matrix[l], matrix[j])
+                matrix_temp = matrix[l:j] + (matrix[l], )
+                matrix = matrix[:l] + (matrix[j], ) + matrix_temp[1:]
+                break
+            if matrix[j][l] != 0 and j != len(matrix) - 1:
+                print(matrix[l], matrix[j])
+                matrix_temp = matrix[l:j] + (matrix[l], ) + matrix[j + 1:]
+                matrix = matrix[:l] + (matrix[j], ) + matrix_temp[1:]
+                break
