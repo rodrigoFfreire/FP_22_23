af5e5c3bd9c727c677b3c7ef670efbedce5f9196
rodrigoFfreire <rodrigofreitasfreire@gmail.com>
2022-10-08 14:51:25 +0100

    updated project - part3 fixed swap alg + func3.2.4

--- Projeto1/Part3 - Sol. Sistema de Eqs Lineares.py
+++ Projeto1/Part3 - Sol. Sistema de Eqs Lineares.py
@@ -9,7 +9,7 @@ def produto_interno(vet1: tuple, vet2: tuple) -> float:
 def verifica_convergencia(matrix: tuple, c: tuple, x: tuple, prec: float) -> bool:
     results = []
     for i in range(len(matrix)):
-        if abs(produto_interno(matrix[i], x) - c[i]):
+        if abs(produto_interno(matrix[i], x) - c[i]) < prec:
             results.append(True)
         else:
             results.append(False)
@@ -17,40 +17,36 @@ def verifica_convergencia(matrix: tuple, c: tuple, x: tuple, prec: float) -> boo
     return all(results)
 
 
-def is_last(matrix: tuple, j: int) -> tuple:
-    if j == len(matrix) - 1:
-        return ()
-    return matrix[j + 1:]
+def swap(i: int, j: int, matrix: tuple) -> tuple:
+    if i > j:
+        return matrix[:j] + (matrix[i], ) + matrix[j + 1:i] + (matrix[j], ) + matrix[i + 1:]
+    return matrix[:i] + (matrix[j], ) + matrix[i + 1:j] + (matrix[i], ) + matrix[j + 1:]
 
 def retira_zeros_diagonal(matrix: tuple, c: tuple) -> tuple:
     matrix_res = matrix
-    matrix_temp = ()
     vector_res = ()
-    for i in range(len(matrix_res)):
+    for i in range(len(matrix)):
         if matrix_res[i][i] == 0:
-            for j in range(i + 1, len(matrix_res)):
-                if matrix_res[j][i] != 0:
-                    matrix_temp = matrix_res[i:j] + (matrix_res[i], ) + is_last(matrix_res, j)
-                    matrix_res = matrix_res[:i] + (matrix_res[j], ) + matrix_temp[1:]
+            for j in range(len(matrix)):
+                if i != j and matrix_res[j][i] != 0 and matrix_res[i][j] != 0:
+                    matrix_res = swap(i, j, matrix_res)
                     break
+                
     for k in matrix_res:
         vector_res += (c[matrix.index(k)], )
         
     return matrix_res, vector_res
 
 
-def eh_diagonal_dominante(matrix: tuple) -> bool:
+def eh_diagonal_dominante(matrix: tuple) -> bool: 
     for i in range(len(matrix)):
-        if abs(matrix[i][i]) <= sum(matrix[i], -matrix[i][i]):
-            print(i, matrix[i])
+        sum_non_diagonal = 0
+        for j in range(len(matrix)):
+            if i != j : sum_non_diagonal += abs(matrix[i][j])
+        if abs(matrix[i][i]) < sum_non_diagonal:
             return False
     return True
 
 
-def resolve_sistema(matriz: tuple, c: tuple, precision: float) -> tuple:
+def resolve_sistema(matriz: tuple, c: tuple, prec: float) -> tuple:
     pass
-
-
-#matrix = ((0, 1, 1, 1), (1, 0, 1, 1), (1, 1, 0, 1), (1, 1, 1, 0))
-matrix = ((1, 0, 0), (0, 1, 0), (0, 1, 2))
-print(eh_diagonal_dominante(matrix))
